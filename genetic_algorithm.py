f0e2f75f7136e46c6753f6e8c15b188f1b32eb88
sheldon05 <jesussantoscapote@gmail.com>
2022-12-25 11:06:36 -0500

    reproduction method finished

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,16 +3,53 @@ import numpy as np
 
 class genetic_algorithm:
 
-    def __init__(self, population_lenght, cost_matrix, beta = 1.6, alpha = 0.15) -> None:
+    def __init__(self, population_lenght, cost_matrix, get_fitness, beta = 1.6, alpha = 0.15) -> None:
         self.cost_matrix = cost_matrix
         self.beta = beta
         self.n_nodes = np.shape(cost_matrix)[0]
         self.population = []
+        self.gen_list = []
+        self.get_fitness = get_fitness
+        self.population_fitnesses = []
 
         for i in range(population_lenght):
             graph = nx.erdos_renyi_graph(self.n_nodes, alpha)
             graph = self.add_edge_weigth(graph)
-            self.population.append(graph)
+            gen = self.get_gen(graph)
+            self.population.append((graph, gen, 0))
+
+
+    def reproduction(self): #TODO check if this work fine because the indexes of the population 
+        p_max = self.beta / len(self.population)
+        p_min = (2 - self.beta) / len(self.population)
+        n = len(self.population)
+        intermediate_population = []
+        p_selection = [] 
+
+        for i in range(n):
+            p_i = p_min + (p_max - p_min)*((n - i)/(n - 1))
+
+            p = p_i * n
+
+            if p >= 1.5: #duplicate the graph
+                intermediate_population.append(self.population[i])
+                intermediate_population.append(self.population[i])
+
+            if p >= 0.5 and p < 1.5: #mantain the graph
+                intermediate_population.append(self.population[i])
+
+        self.population = intermediate_population
+        
+
+
+
+
+    def calculate_population_fitnesses(self):
+        for item in self.population:
+            fitness = self.get_fitness(item[0])
+            item[2] = fitness
+
+        self.population.sort(key= lambda x: x[2], reverse=True)     
 
 
     def  add_edge_weigth(self, graph : nx.Graph):
@@ -50,4 +87,6 @@ class genetic_algorithm:
 #     print(u)
 #     print(v)  
 
-print(not 0)
+a = [3, 5, 6, 6, 4]
+a.sort()
+print(a)
